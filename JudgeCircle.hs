fa6ef4dc899690f011e878612535e92b1e263a61
Pavithran <pavithranrao.1512@gmail.com>
2018-04-11 21:52:17 -0400

    JudgeCircle LeetCode

--- JudgeCircle.hs
+++ JudgeCircle.hs
@@ -1,24 +1,23 @@
 -- https://leetcode.com/problems/judge-route-circle/
-
 module Main where
 
 import Data.List
 
-
-judgeCircle moves = let
-                      (effHor, effVer) = getEffectiveMoves moves
-                    in effHor == 0 && effVer == 0
-
+judgeCircle moves = getEffectiveMoves moves == (0, 0)
+-- judgeCircle moves = let
+--                       (effHor, effVer) = getEffectiveMoves moves
+--                     in effHor == 0 && effVer == 0
 getEffectiveMoves moves = foldl (\(accVer, accHor) move ->
                                       case move of
                                         'U' -> (accVer + 1, accHor)
                                         'D' -> (accVer - 1, accHor)
                                         'L' -> (accVer, accHor + 1)
-                                        'R' -> (accVer, accHor - 1)) (0, 0) moves
+                                        'R' -> (accVer, accHor - 1))
+                                (0, 0) moves
 
 main :: IO ()
 main = do
-  let moves = "RLUURDDDLU"
+  let moves = "RLUURDDDLULL"
   let isCircle = judgeCircle moves
   if isCircle
     then print ("The moves " ++ moves ++ " make a circle")
